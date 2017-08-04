35f4e8487de1705811b34b7d9b1793a23ac9fbb9
Juan Carlos Orrego <jcorrego@gmail.com>
2017-08-04 09:03:40 -0500

    Spanish texts

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Las contraseñas deben tener al menos seis caracteres y coincidir con la confirmación.',
+    'reset' => 'Tu contraseña se ha cambiado!',
+    'sent' => 'Hemos enviado un link para reasignar la contraseña a tu correo',
+    'token' => 'Este token para reasignar contraseña no es válido.',
+    'user' => "No hemos encontrado un usuario con ese correo.",
+
+];
