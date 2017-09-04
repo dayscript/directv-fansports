1736ba9966523d9b7e64bce85c340a01e07114da
jcardenas2015 <jcardenas@dayscript.com>
2017-09-04 17:50:30 -0500

    pronosticos movil

--- public/js/app.js
+++ public/js/app.js
@@ -21259,11 +21259,11 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
     }, [_c('strong', {
       staticClass: "show-for-small-only"
     }, [_vm._v("Marcador: ")]), _vm._v("\n                    " + _vm._s(match.local_score) + " - " + _vm._s(match.visit_score) + "\n                ")]), _vm._v(" "), _c('div', {
-      staticClass: "small-4 medium-1 columns text-center puntaje"
+      staticClass: "small-6 medium-1 columns text-center puntaje"
     }, [_c('strong', {
       staticClass: "show-for-small-only"
     }, [_vm._v("Puntos: ")]), _vm._v(_vm._s(match.points))]), _vm._v(" "), _c('div', {
-      staticClass: "small-4 medium-1 columns canal text-right"
+      staticClass: "small-12 medium-1 columns canal text-right"
     }, [_c('img', {
       staticClass: "logo",
       attrs: {
--- resources/assets/js/components/game/Play.vue
+++ resources/assets/js/components/game/Play.vue
@@ -93,8 +93,8 @@
                     <div class="small-6 medium-1 columns text-center marcador"><strong class="show-for-small-only">Marcador: </strong>
                         {{ match.local_score }} - {{ match.visit_score }}
                     </div>
-                    <div class="small-4 medium-1 columns text-center puntaje"><strong class="show-for-small-only">Puntos: </strong>{{ match.points }}</div>
-                    <div class="small-4 medium-1 columns canal text-right">
+                    <div class="small-6 medium-1 columns text-center puntaje"><strong class="show-for-small-only">Puntos: </strong>{{ match.points }}</div>
+                    <div class="small-12 medium-1 columns canal text-right">
                         <img class="logo" :src="'/img/channels/'+ match.channel +'.png'" alt="DirecTV">{{ match.channel }}
                     </div>
                 </div>
