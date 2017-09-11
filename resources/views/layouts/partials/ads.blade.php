f0cc2be418653036e8f4952f2cc19fa70ac5cece
jcardenas2015 <jcardenas@dayscript.com>
2017-09-11 10:06:16 -0500

    error like facebook

--- resources/views/layouts/partials/ads.blade.php
+++ resources/views/layouts/partials/ads.blade.php
@@ -1,7 +1,2 @@
 <img src="{{ asset('img/ads/servientrega.jpg') }}" alt="" class="Servientrega">
 <hr class="light">
-<div>
-    <iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.servientregafutbolfans.com.co&width=189&layout=button_count&action=like&size=large&show_faces=false&share=true&height=46&appId=192244770841253"
-            width="189" height="46" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-            allowTransparency="true"></iframe>
-</div>
