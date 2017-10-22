3a3877e2c24207ecd8dde4cf93833576e6fe0651
Juan Carlos Orrego <jcorrego@gmail.com>
2017-10-22 18:15:51 -0500

    Fix Facebook login

--- app/Http/Middleware/CheckCity.php
+++ app/Http/Middleware/CheckCity.php
@@ -16,7 +16,12 @@ class CheckCity
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        if(($user = auth()->user()) && (!$user->city) && !$request->is('actualizar-ciudad') && !$request->is('users/'.$user->id)){
+        if(($user = auth()->user())
+           && (!$user->city)
+           && !$request->is('actualizar-ciudad')
+           && !$request->is('users/'.$user->id)
+           && !$request->is('login/*')
+        ){
             return redirect('actualizar-ciudad')->with('redirect','/'.$request->path());
 //            request()->
 //            dd(auth()->user());
