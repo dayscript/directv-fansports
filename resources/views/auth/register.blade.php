d45a63a21acbb39da4021f1561a6dd2fc626e86b
jcardenas2015 <jcardenas@dayscript.com>
2017-11-02 10:45:40 -0500

    cambios form registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,15 +12,13 @@
         <div class="row">
             <div class="medium-9 columns">
                 <p>Puede registrarse o iniciar sesión usando su cuenta de Facebook</p>
-                <a href="/login/facebook" class="button facebook"><i class="fi-social-facebook"></i> Iniciar sesión en
-                    Facebook</a>
+                <a href="/login/facebook" class="button facebook"><i class="fi-social-facebook"></i> Iniciar sesión en Facebook</a>
                 <hr class="green">
                 <div class="row">
                     <div class="medium-6 columns">
                         <label>Tipo de identificación
                             @php($types = collect(['CC'=>'Cédula de ciudadanía','CE'=>'Cédula de extranjería','PASSPORT'=>'Pasaporte']))
-                            <select name="identification_type"
-                                    class="{{ $errors->has('identification_type') ? ' is-invalid-input' : '' }}">
+                            <select name="identification_type" class="{{ $errors->has('identification_type') ? ' is-invalid-input' : '' }}">
                                 @foreach($types as $key=>$type)
                                     <option {{ ($key==old('identification_type','CC'))?'selected':'' }} value="{{ $key }}">{{ $type }}</option>
                                 @endforeach
@@ -33,9 +31,7 @@
                     <div class="medium-6 columns">
                         <label>
                             Identificación
-                            <input type="text" name="identification"
-                                   class="{{ $errors->has('identification') ? ' is-invalid-input' : '' }}"
-                                   value="{{ old('identification') }}" required autofocus>
+                            <input type="text" name="identification" class="{{ $errors->has('identification') ? ' is-invalid-input' : '' }}" value="{{ old('identification') }}" required autofocus>
                             @if ($errors->has('identification'))
                                 <span class="form-error is-visible">{{ $errors->first('identification') }}</span>
                             @endif
@@ -46,8 +42,7 @@
                     <div class="medium-6 columns">
                         <label>
                             Nombres
-                            <input type="text" name="name" class="{{ $errors->has('name') ? ' is-invalid-input' : '' }}"
-                                   value="{{ old('name') }}" required autofocus>
+                            <input type="text" name="name" class="{{ $errors->has('name') ? ' is-invalid-input' : '' }}" value="{{ old('name') }}" required autofocus>
                             @if ($errors->has('name'))
                                 <span class="form-error is-visible">{{ $errors->first('name') }}</span>
                             @endif
@@ -56,8 +51,7 @@
                     <div class="medium-6 columns">
                         <label>
                             Apellidos
-                            <input type="text" name="last" class="{{ $errors->has('last') ? ' is-invalid-input' : '' }}"
-                                   value="{{ old('last') }}">
+                            <input type="text" name="last" class="{{ $errors->has('last') ? ' is-invalid-input' : '' }}" value="{{ old('last') }}">
                             @if ($errors->has('last'))
                                 <span class="form-error is-visible">{{ $errors->first('last') }}</span>
                             @endif
@@ -68,9 +62,7 @@
                     <div class="medium-6 columns">
                         <label>
                             Email
-                            <input type="email" name="email"
-                                   class="{{ $errors->has('email') ? ' is-invalid-input' : '' }}"
-                                   value="{{ old('email') }}" required>
+                            <input type="email" name="email" class="{{ $errors->has('email') ? ' is-invalid-input' : '' }}" value="{{ old('email') }}" required>
                             @if ($errors->has('email'))
                                 <span class="form-error is-visible">{{ $errors->first('email') }}</span>
                             @endif
@@ -79,9 +71,7 @@
                     <div class="medium-6 columns">
                         <label>
                             Celular
-                            <input type="text" name="phone"
-                                   class="{{ $errors->has('phone') ? ' is-invalid-input' : '' }}"
-                                   value="{{ old('phone') }}">
+                            <input type="text" name="phone" class="{{ $errors->has('phone') ? ' is-invalid-input' : '' }}" value="{{ old('phone') }}">
                             @if ($errors->has('phone'))
                                 <span class="form-error is-visible">{{ $errors->first('phone') }}</span>
                             @endif
@@ -89,7 +79,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    {{--<div class="medium-6 columns">
+                    <div class="medium-6 columns">
                         <label>Pais
                             @php($countries = App\Http\Utilities\Country::all())
                             <select name="country" class="{{ $errors->has('country') ? ' is-invalid-input' : '' }}">
@@ -101,7 +91,7 @@
                                 <span class="form-error is-visible">{{ $errors->first('country') }}</span>
                             @endif
                         </label>
-                    </div>--}}
+                    </div>
                     <div class="medium-6 columns">
                         <label>Ciudad
                             <select name="city">
@@ -109,9 +99,9 @@
                                 <option value="" {{ old('city','Bogotá')==''?'selected':'' }}>Seleccione</option>
                                 @foreach($cities as $key=>$dept)
                                     <optgroup label="{{ $key }}">
-                                        @foreach($dept as $city)
-                                            <option value="{{ $city }}" {{ old('city','Bogotá')==$city?'selected':'' }}>{{ $city }}</option>
-                                        @endforeach
+                                    @foreach($dept as $city)
+                                        <option value="{{ $city }}" {{ old('city','Bogotá')==$city?'selected':'' }}>{{ $city }}</option>
+                                    @endforeach
                                     </optgroup>
                                 @endforeach
                             </select>
@@ -120,42 +110,28 @@
                             @endif
                         </label>
                     </div>
+                </div>
+                <div class="row">
                     <div class="medium-6 columns">
                         <label>
                             Contraseña
-                            <input type="password" name="password"
-                                   class="{{ $errors->has('password') ? ' is-invalid-input' : '' }}" required>
+                            <input type="password" name="password" class="{{ $errors->has('password') ? ' is-invalid-input' : '' }}" required>
                             @if ($errors->has('password'))
                                 <span class="form-error is-visible">{{ $errors->first('password') }}</span>
                             @endif
                         </label>
                     </div>
-                </div>
-                <div class="row">
-                    {{--}}<div class="medium-6 columns">
-                        <label>
-                            Contraseña
-                            <input type="password" name="password"
-                                   class="{{ $errors->has('password') ? ' is-invalid-input' : '' }}" required>
-                            @if ($errors->has('password'))
-                                <span class="form-error is-visible">{{ $errors->first('password') }}</span>
-                            @endif
-                        </label>
-                    </div>--}}
                     <div class="medium-6 columns">
                         <label>
                             Confirmar contraseña
-                            <input id="password-confirm" type="password" class="form-control"
-                                   name="password_confirmation" required>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                         </label>
                     </div>
                 </div>
                 <fieldset class="row columns hide">
                     <legend>Género</legend>
-                    <input type="radio" name="gender" value="M" id="gender_m" {{ old('gender','M')=='M'?'checked':'' }}><label
-                            for="gender_m">Masculino</label>
-                    <input type="radio" name="gender" value="F" id="gender_f" {{ old('gender','M')=='F'?'checked':'' }}><label
-                            for="gender_f">Femenino</label>
+                    <input type="radio" name="gender" value="M" id="gender_m" {{ old('gender','M')=='M'?'checked':'' }}><label for="gender_m">Masculino</label>
+                    <input type="radio" name="gender" value="F" id="gender_f" {{ old('gender','M')=='F'?'checked':'' }}><label for="gender_f">Femenino</label>
                     @if ($errors->has('gender'))
                         <span class="form-error is-visible">{{ $errors->first('gender') }}</span>
                     @endif
@@ -166,15 +142,12 @@
                     <p>¿Le gustaría estar al día en las emocionantes promociones ofrecidas por Directv y Servientrega?
                         Puede cambiar esta configuración en las preferencias de su perfil en cualquier momento.
                     </p>
-                    <input id="promotions" name="promotions" type="checkbox"
-                           {{ old('promotions')?'checked':'' }} value="1"><label for="promotions">Si, deseo recibir
-                        promociones</label>
+                    <input id="promotions" name="promotions" type="checkbox" {{ old('promotions')?'checked':'' }} value="1"><label for="promotions">Si, deseo recibir promociones</label>
                 </div>
                 <hr>
                 <hr class="light">
                 <div class="row columns">
-                    <p>Al continuar estás aceptando los <a target="_blank" href="/terminos-y-condiciones">términos y
-                            condiciones</a> de la plataforma.</p>
+                    <p>Al continuar estás aceptando los <a target="_blank" href="/terminos-y-condiciones">términos y condiciones</a> de la plataforma.</p>
                 </div>
                 <button type="submit" class="button expanded">Aceptar y continuar</button>
             </div>
