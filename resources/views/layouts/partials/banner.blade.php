6165bc4169983b508698f21f00655e63d04a4b7c
jcardenas2015 <jcardenas@dayscript.com>
2017-11-02 09:46:00 -0500

    cambios

--- resources/views/layouts/partials/banner.blade.php
+++ resources/views/layouts/partials/banner.blade.php
@@ -8,7 +8,7 @@
                         <figcaption class="orbit-caption text-left">
                             <h5 class="seccion hide-for-small-only">Premio Mayor</h5>
                             <h2 class="banner">Vive las emociones del partido Real Madrid VS Sevilla en Madrid</h2>
-                            <a href="premios" class="button hide-for-small-only">Ver más</a>
+                            <a href="register" class="button hide-for-small-only">Regístrate aqui y participa</a>
                         </figcaption>
                     </div>
                 </figure>
@@ -20,7 +20,7 @@
                         <figcaption class="orbit-caption text-left">
                             <h5 class="seccion hide-for-small-only">Premios Semana Servientrega</h5>
                             <h2 class="banner">Gánate una camiseta oficial de la Selección Colombia</h2>
-                            <a href="register" class="button hide-for-small-only">Regístrate aquí y participa</a>
+                            <a href="premios" class="button hide-for-small-only">Ver más</a>
                         </figcaption>
                     </div>
                 </figure>
