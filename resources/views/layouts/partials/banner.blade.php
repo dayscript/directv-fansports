1a7c6638550256b35e157a64cc56e0a71c974240
jcardenas2015 <jcardenas@dayscript.com>
2017-11-02 10:17:26 -0500

    cambios

--- resources/views/layouts/partials/banner.blade.php
+++ resources/views/layouts/partials/banner.blade.php
@@ -19,7 +19,7 @@
                     <div class="row">
                         <figcaption class="orbit-caption text-left">
                             <h5 class="seccion hide-for-small-only">Premios Semana Servientrega</h5>
-                            <h2 class="banner">Gánate una camiseta oficial de la Selección Colombia</h2>
+                            <h2 class="banner hide-for-small-only">Gánate una camiseta oficial de la Selección Colombia</h2>
                             <a href="premios" class="button hide-for-small-only">Ver más</a>
                             <a href="register" class="button show-for-small-only">Regístrate aquí y participa</a>
                         </figcaption>
