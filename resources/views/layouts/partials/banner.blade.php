b702a154b30813cfba2457270330479435f89122
jcardenas2015 <jcardenas@dayscript.com>
2017-11-14 11:19:33 -0500

    ajustes

--- resources/views/layouts/partials/banner.blade.php
+++ resources/views/layouts/partials/banner.blade.php
@@ -3,17 +3,20 @@
         <ul class="orbit-container">
             <li class="is-active orbit-slide">
                 <figure class="orbit-figure">
-                    <img class="orbit-image" src="{{ asset('img/banners/slide.jpg') }}" alt="Banner">
+                    <a href="premios">
+                        <img class="orbit-image" src="{{ asset('img/banners/ganadores.png') }}" alt="Banner">
+                    </a>
+
                     <div class="row">
                         <figcaption class="orbit-caption text-left">
-                            <h5 class="seccion hide-for-small-only">Premio Mayor</h5>
-                            <h2 class="banner">Vive las emociones del partido Real Madrid VS Sevilla en Madrid</h2>
-                            <a href="register" class="button hide-for-small-only">Regístrate aquí y participa</a>
+                            <!--<h5 class="seccion hide-for-small-only"> </h5>
+                            <h2 class="banner"></h2>
+                            <a href="ganadores" class="button hide-for-small-only">Regístrate aquí y participa</a>-->
                         </figcaption>
                     </div>
                 </figure>
             </li>
-            <li class="orbit-slide">
+          <!--  <li class="orbit-slide">
                 <figure class="orbit-figure">
                     <img class="orbit-image" src="{{ asset('img/banners/slide2.jpg') }}" alt="Banner">
                     <div class="row">
@@ -25,7 +28,7 @@
                         </figcaption>
                     </div>
                 </figure>
-            </li>
+            </li>-->
         </ul>
     </div>
     <nav class="orbit-bullets hide-for-small-only">
